9961f25718fd125ca9539f8cee233175878052b2
Jason Bourget <jason.andre.bourget@gmail.com>
2019-03-21 20:04:40 -0600

    Removed node-postgres code

--- database-sdc/seed.js
+++ database-sdc/seed.js
@@ -15,48 +15,5 @@ const seedDescriptions = async (qty, batches) => {
   }
 }
 
-
-// const seedDescriptions = (qty, batches) => {
-//   const descriptions = generateDescriptions(qty/batches);
-//   return Description.sync({force: true})
-//   .then(() => {
-//     return Description.bulkCreate(descriptions)
-//   })
-//   .then(() => {
-//     const descriptions = generateDescriptions(qty/batches);
-//     return Description.bulkCreate(descriptions)
-//   })
-// }
-
-/* Using node-postgres instead */
-const { Client } = require('pg');
-const client = new Client();
-
-const convertArrayToSQL = (arr) => {
-  return arr.reduce((accum, val, index) => {
-    return accum += arr[index + 1] ? `"${val}",` : `"${val}"}'`;
-  },`'{`);
-}
-
-const createDescriptionsQuery = (qty) => {
-  let queryString = 'INSERT INTO descriptions (text, categories, likes) VALUES '
-  const descriptions = generateDescriptions(qty);
-  descriptions.forEach((description, index) => {
-    let punc = descriptions[index + 1] ? ',' : ';';
-    let { text, categories, likes } = description;
-    categories = convertArrayToSQL(categories);
-    queryString += `('${text}', ${categories}, ${likes})${punc}`;
-  });
-  return queryString;
-}
-
-const seedDescriptionsPG = async (qty) => {
-  let queryString = createDescriptionsQuery(qty);
-  await client.connect();
-  await client.query(queryString);
-  client.end();
-}
-
-
 // seedDescriptions(1);
 seedDescriptions(10000000, 100);
