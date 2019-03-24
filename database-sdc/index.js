9ef92103bf4c235f0b7e73459446591dcbd9698a
Jason Bourget <jason.andre.bourget@gmail.com>
2019-03-24 15:52:34 -0600

    Completed Postgres seeding using Copy query and streams

--- database-sdc/index.js
+++ database-sdc/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('sdc', 'postgres', 'postgres', {
+const sequelize = new Sequelize('sdc2', 'postgres', 'postgres', {
   host: 'localhost',
   dialect: 'postgres',
   logging: false
--- database-sdc/seed.js
+++ database-sdc/seed.js
@@ -1,46 +1,37 @@
 require('dotenv').config();
+const fs = require('fs')
+const path = require('path')
+const { Pool, Client } = require('pg')
+const copyFrom = require('pg-copy-streams').from;
+const srcDir = path.resolve(__dirname, '../data');
+const pool = new Pool();
+const hirestime = require('hirestime');
+const prettyMs = require('pretty-ms');
 
-const { generateDescriptions, generateComments, generateUsers } = require('./fakes.js');
 const { Description, Comment, User } = require('./index.js');
 
-/* Seeds the desired quantity of descriptions in a desired batch size.
-Suggested 100,000 records/batch */
-const seedDescriptions = async (qty = 10000000, batchSize = 100000) => {
-  await Description.sync({force: true});
-  let batches = Math.floor(qty / batchSize) + 1;
-  let startingVideoId = 1;
-  while(batches > 0) {
-    let descriptions = generateDescriptions(batchSize, startingVideoId);
-    await Description.bulkCreate(descriptions);
-    batches --;
-    startingVideoId += batchSize;
-  }
+/* Streams contents of CSV file into database */
+const seedFromCSV = async (Model, table) => {
+  const getElapsed = hirestime();
+
+  await Model.sync({force: true});
+  const client = await pool.connect();
+  const writeStream = client.query(copyFrom(`COPY ${table} FROM STDIN CSV HEADER`));
+  const readStream = fs.createReadStream(`${srcDir}/${table}.csv`);
+
+  writeStream.on('end', () => {
+    const elapsed = prettyMs(getElapsed(hirestime.MS));
+    console.log(`Finished seeding the ${table} table in ${elapsed}!`);
+    client.release();
+  });
+
+  return readStream.pipe(writeStream);
 }
 
-/* Seeds the desired quantity of users, not using batches.
-There are probably fewer users than primary records, so defaults to 100k records */
-const seedUsers = async (qty = 100000) => {
-  await User.sync({force: true});
-  const users = generateUsers(qty);
-  return User.bulkCreate(users);
+const seed = async () => {
+  await seedFromCSV(User, 'users');
+  await seedFromCSV(Description, 'descriptions');
+  await seedFromCSV(Comment, 'comments');
 }
 
-const seedComments = async (videoQty = 10000000, batchSize = 100000) => {
-  await Comment.sync({force: true});
-  let start = 1;
-  let batches = Math.floor(videoQty / batchSize) + 1;
-  while(batches > 0) {
-    let comments = generateComments(start, batchSize);
-    await Comment.bulkCreate(comments);
-    batches --;
-    start += batchSize;
-  }
-}
-
-const seed = async (qty, batchSize) => {
-  await seedDescriptions(qty, batchSize);
-  await seedComments(qty, batchSize);
-  await seedUsers();
-}
-
-seed(10000000, 100000);
+seed();
