7782d62729c907bd8c510cd24252023b4084dc84
Jason Bourget <jason.andre.bourget@gmail.com>
2019-04-16 19:20:45 -0600

    Modified React URL config file

--- client/config.js
+++ client/config.js
@@ -1,28 +1,6 @@
-/* Environment defaults to production if none is specified */
+/* Generates the API URL used by the frontend,
+based on the Node environment. */
+
 const env = process.env.NODE_ENV || 'production';
-
-/* Configuration bindings for each environment.
-Just set the NODE_ENV when the environment is initialized,
-and the relevant configuration will be exported from this file. */
-
-const development = {
-  port: process.env.PORT_DEV,
-  host: process.env.HOST_DEV
-};
-
-const production = {
-  port: process.env.PORT,
-  host: process.env.HOST
-}
-
-/* Exporting the relevant settings from the file,
-based on NODE_ENV */
-
-const config = {
- development,
- production
-};
-
-const url = config[env].port ? `${config[env].host}:${config[env].port}` : `${config[env].host}`;
-
+const url = env === 'production' ? process.env.HOST : process.env.HOST_DEV + ':' + process.env.PORT_DEV;
 export default url;
